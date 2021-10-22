c3997a06bacfe43d8fb0e99679af6c7f9206e803
Marc Ranger <marc.m.ranger@gmail.com>
2021-10-22 11:58:14 -0600

    first commit

---
+++ amazonclone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD8S-NMV6K4PuKPbKcAmCRqWqLyuUuNz40",
+  authDomain: "challenge-e818e.firebaseapp.com",
+  projectId: "challenge-e818e",
+  storageBucket: "challenge-e818e.appspot.com",
+  messagingSenderId: "73740026066",
+  appId: "1:73740026066:web:5ec04c1a92833ca6d6ed0b"
+};
